eb7f22019240f4d709f3ab74bc7425e966d95afd
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2024-04-21 11:13:52 +0530

    Update ai.py

--- AnonXMusic/plugins/tools/ai.py
+++ AnonXMusic/plugins/tools/ai.py
@@ -28,7 +28,7 @@ async def chat_gpt(bot, message):
                     end_time = time.time()
                     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
                     await message.reply_text(
-                         f"❖ {x}  \n\n❖ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➥  ๛ᴀ ᴠ ɪ s ʜ ᴀ ࿐",
+                         f"❖ {x}  \n\n❖ ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➥  ๛ᴀ ɴ ᴋ ɪ ᴛ ᴀ ࿐",
                         parse_mode=ParseMode.MARKDOWN
                     )
                 else:
