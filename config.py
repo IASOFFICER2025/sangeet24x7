613073b7a31368c3b20814f53e8342e0c713fafa
Anuj Kumar RoY <106427006+anujkumarroy@users.noreply.github.com>
2024-04-21 10:56:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/LOCO-PILOT/ABC",
+    "https://github.com/IASOFFICER2025/sangeet24x7",
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
